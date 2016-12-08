6aa0a9346d1552d0651f22d7ecf3153907a94b02
carvendy <1150483011@qq.com>
2016-12-08 22:19:43 +0800

    现在开始提交

--- src/main/scala/com/carvendy/hello/scala/TestRational.scala
+++ src/main/scala/com/carvendy/hello/scala/TestRational.scala
@@ -16,21 +16,36 @@ class Rational(a:Int,b:Int){
    */
   override def toString = a +"/" + b
   
-  def add(that:Rational): Rational = {
+  def +(that:Rational): Rational = {
     
     println((a*that.denom + that.numer * b) + "/"+(b*that.denom))
     return null;
   }
   
+  
+   def +(n:Int): Rational={
+   // println((a + b *n) +"/"+b)
+    return new Rational((a + b *n),b)
+  }
+   
+  
 }
 
 object TestRational extends App {
+  
+  /**
+   *  只能在解释器里面定义
+   */
+  implicit def intToRational(n:Int) = new Rational(n)
   
   val one = new Rational(1,2)
  // val two = new Rational(1,0)// 抛异常
   println(one)
   
   val that = new Rational(1,4)
-  one.add(that);
-  
+  //(one + that);
+   
+   //one + 1
+   1 + one
+   
 }
